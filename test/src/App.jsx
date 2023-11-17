240d393b15b41056002673b9cdb4dc67becc1f31
Ana María Garzón <anamari.garzon@urosario.edu.co>
2023-11-17 11:57:24 -0500

    organize header footer

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
     <Header></Header>
+    <Footer></Footer>
     </>
   )
 }
